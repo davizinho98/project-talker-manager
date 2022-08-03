29e55d2ae3c6a84c4818a9a6299c2a354a93b1e5
davi <daviseles99@gmail.com>
2022-08-03 14:39:47 -0500

    feat: implementa rotas de talker e talker id

--- routes/talkerRoutes.js
+++ routes/talkerRoutes.js
@@ -2,6 +2,28 @@ const express = require('express');
 
 const talkerRouter = express.Router();
 
+const { readTalkers } = require('../utils/index');
+
+talkerRouter.get('/', async (_req, res) => {
+  const talkers = await readTalkers();
+
+  res.status(200).json(talkers);
+});
+
+talkerRouter.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readTalkers();
+
+  const talkerById = talkers
+    .find((talker) => talker.id === Number(id));
+
+  if (!talkerById) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+
+  res.status(200).json(talkerById);
+});
+
 module.exports = {
   talkerRouter,
 };

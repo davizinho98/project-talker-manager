d3c8363ad54632294cc58fd0b44fc54502743317
davi <daviseles99@gmail.com>
2022-08-03 18:58:32 -0500

    feat: adiciona middlewares para validar dados do novo talker

---
+++ middlewares/validateDataTalker.js
@@ -0,0 +1,74 @@
+const validateName = (req, res, next) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ message: 'O campo "name" é obrigatório' });
+  }
+
+  if (name.length < 3) {
+    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+
+  next();
+};
+
+const validateAge = (req, res, next) => {
+  const { age } = req.body;
+
+  if (!age || age === '') {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+
+  if (Number(age) < 18) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+
+  next();
+};
+
+const validateTalk = (req, res, next) => {
+  const { talk } = req.body;
+  if (!talk) {
+    return res.status(400).json({ message: 'O campo "talk" é obrigatório' });
+  }
+
+  next();
+};
+
+const validateWatchedAt = (req, res, next) => {
+  const { talk } = req.body;
+
+  const dateRegex = /^\d{2}\/\d{2}\/\d{4}$/;
+
+  if (!talk.watchedAt || talk.watchedAt === '') {
+    return res.status(400).json({ message: 'O campo "watchedAt" é obrigatório' });
+  }
+
+  if (!dateRegex.test(talk.watchedAt)) {
+    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+
+  next();
+};
+
+const validateRate = (req, res, next) => {
+  const { talk } = req.body;
+
+  if (!talk.rate || talk.rate === '') {
+    return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
+  }
+
+  if (Number(talk.rate) > 5 || Number(talk.rate) < 1) {
+    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+
+  next();
+};
+
+module.exports = {
+  validateName,
+  validateAge,
+  validateTalk,
+  validateWatchedAt,
+  validateRate,
+};

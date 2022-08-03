2d0d9b43f79bc180e4dc20928ce7183af06312b5
davi <daviseles99@gmail.com>
2022-08-03 15:03:43 -0500

    feat: adiciona rota de login que retorna token

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,3 +1,15 @@
 const express = require('express');
+const { generateToken } = require('../utils');
 
 const loginRouter = express.Router();
+
+loginRouter.post('/', (_req, res) => {
+  /* const { email, password } = req.body; */
+  const tokenObj = { token: generateToken() };
+
+  res.status(200).json(tokenObj);
+});
+
+module.exports = {
+  loginRouter,
+};

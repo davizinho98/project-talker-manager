f70d5fffdad49ff1830eda70f16ae22bf7a9ec25
davi <daviseles99@gmail.com>
2022-08-03 20:09:56 -0500

    feat: adiciona rota delete para deletar talker

--- routes/talkerRoutes.js
+++ routes/talkerRoutes.js
@@ -76,9 +76,22 @@ talkerRouter
     .findIndex((talker) => talker.id === Number(id));
   talkers[talkerIndex] = { ...talkers[talkerIndex], name, age, talk: { watchedAt, rate } };
   await writeTalkers(talkers);
+
   res.status(200).json(talkers[talkerIndex]);
 },
 );
+
+talkerRouter.delete('/:id', validateTokenUser, async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readTalkers();
+  const talkerIndex = talkers
+    .findIndex((talker) => talker.id === Number(id));
+  
+  talkers.splice(talkerIndex, 1);
+  await writeTalkers(talkers);
+
+  res.status(204).end();
+});
 
 module.exports = {
   talkerRouter,

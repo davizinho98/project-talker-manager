a5bc4b2ce65024c3c62eed0dc7d2822517e91547
davi <daviseles99@gmail.com>
2022-08-10 08:55:47 -0500

    commit teste

--- middlewares/validateDataTalker.js
+++ middlewares/validateDataTalker.js
@@ -28,6 +28,7 @@ const validateAge = (req, res, next) => {
 
 const validateTalk = (req, res, next) => {
   const { talk } = req.body;
+
   if (!talk) {
     return res.status(400).json({ message: 'O campo "talk" é obrigatório' });
   }
